505f561c95addcc22f950e9c2d452c3a7056b525
Jason Richards <43837641+jdr145@users.noreply.github.com>
2020-11-07 15:55:30 -0500

    Quicksort OpenMP implementation
    Added the quicksort with OpenMP implementation in C, the batch job script to run on variable number of OMP threads, and the batch job's output

---
+++ quicksortOMP.c
@@ -0,0 +1,78 @@
+/* C implementation QuickSort with OpenMP */
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+// A utility function to swap two elements
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+/* This function takes last element as pivot, places
+the pivot element at its correct position in sorted
+	array, and places all smaller (smaller than pivot)
+to left of pivot and all greater elements to right
+of pivot */
+int partition (int arr[], int low, int high)
+{
+	int pivot = arr[high]; // pivot
+	int i = (low - 1); // Index of smaller element
+	int j;
+	for (j = low; j <= high- 1; j++)
+	{
+		// If current element is smaller than the pivot
+		if (arr[j] < pivot)
+		{
+			i++; // increment index of smaller element
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		/* pi is partitioning index, arr[p] is now
+		at right place */
+		int pi = partition(arr, low, high);
+
+		// Separately sort elements before
+		// partition and after partition
+		#pragma omp task default(none) firstprivate(arr, low, pi)
+		{ quickSort(arr, low, pi - 1); }
+		#pragma omp task default(none) firstprivate(arr, high, pi)
+		{ quickSort(arr, pi + 1, high); }
+	}
+}
+
+// Driver program to test above functions
+int main()
+{
+	int n = 40000000;
+	int *arr;
+	arr = (int *)malloc(sizeof(int)*n);
+
+	// Fill array with random numbers
+	srand(0);
+	int i;
+	for (i = 0; i < n; i++) {
+		arr[i] = (rand() % n); // random numbers between 1 and n
+	}
+
+	#pragma omp parallel default(none) shared(arr, n)
+	{
+		#pragma omp single nowait
+		{ quickSort(arr, 0, n-1); }
+	}
+	return 0;
+}
